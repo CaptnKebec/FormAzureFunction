d3450c79d6069f3527208c4eb9d640a77485a1dc
Michel Bernier <michel.bernier@hema-quebec.qc.ca>
2023-12-12 10:13:48 -0500

    test1

--- FormAzureFunction/Function1.cs
+++ FormAzureFunction/Function1.cs
@@ -38,7 +38,7 @@ namespace FormAzureFunction
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {name}. Action OK.";
 
             return new OkObjectResult(responseMessage);
         }
