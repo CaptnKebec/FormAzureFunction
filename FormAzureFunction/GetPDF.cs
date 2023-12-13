f57adae9184707586732df9ca832bc1185783a55
Michel Bernier <michel.bernier@hema-quebec.qc.ca>
2023-12-13 13:56:49 -0500

    modif

--- FormAzureFunction/GetPDF.cs
+++ FormAzureFunction/GetPDF.cs
@@ -135,7 +135,7 @@ namespace FormAzureFunction
                     { "x-allow", allowURL }
                 },
                     Method = HttpMethod.Get,
-                    RequestUri = new Uri("https://hq-dev-formioapi-wa.hq.intra/project/" +
+                    RequestUri = new Uri("https://devformioapi.hema-quebec.qc.ca/project/" +
                     submissionProjectId +
                     "/" +
                     "token")
